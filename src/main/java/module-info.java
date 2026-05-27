c35df85aa6f85acbbaee3970442dd88554773529
josh70 official <joshuelricchiardi@gmail.com>
2026-05-27 19:27:05 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.unito.progiii.progiiiclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens it.unito.progiii.progiiiclient to javafx.fxml;
+    exports it.unito.progiii.progiiiclient;
+}

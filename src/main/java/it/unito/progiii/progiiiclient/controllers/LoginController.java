488f4021eb8a441bb0560ca05f3cd165240c4a07
JoshRik <joshuelricchiardi@gmail.com>
2026-05-28 10:30:31 +0200

    implemented constants and email regex

--- src/main/java/it/unito/progiii/progiiiclient/controllers/LoginController.java
+++ src/main/java/it/unito/progiii/progiiiclient/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package it.unito.progiii.progiiiclient.controllers;
 
+import it.unito.progiii.progiiiclient.utils.Constants;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LoginController {
@@ -9,8 +11,13 @@ public class LoginController {
     private TextField emailInput;
 
     @FXML
-    private void submit() {
+    private Label errorLabel;
 
+    @FXML
+    private void submit() {
+        String input = emailInput.getText();
+        if(!input.matches(Constants.EMAIL_REGEX))
+            errorLabel.setText("Indirizzo email non valido");
     }
 
 }
--- src/main/java/it/unito/progiii/progiiiclient/utils/Constants.java
+++ src/main/java/it/unito/progiii/progiiiclient/utils/Constants.java
@@ -2,6 +2,7 @@ package it.unito.progiii.progiiiclient.utils;
 
 public class Constants {
 
-    public static final String EMAIL_REGEX = "[a-zA-z]@[a-zA-Z].[a-zA-z]";
+    public static final int PORT=5000;
+    public static final String EMAIL_REGEX = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/";
 
 }

5c60f32a6be464e5813a2f19a382c2a5d7656414
JoshRik <joshuelricchiardi@gmail.com>
2026-06-01 00:26:16 +0200

    finished pull task and started implementing message details view

--- src/main/java/it/unito/progiii/progiiiclient/network/PullTask.java
+++ src/main/java/it/unito/progiii/progiiiclient/network/PullTask.java
@@ -2,29 +2,60 @@ package it.unito.progiii.progiiiclient.network;
 
 import it.unito.progiii.progiiiclient.model.Email;
 import it.unito.progiii.progiiiclient.state.StateManager;
+import it.unito.progiii.progiiiclient.utils.ConnectUtils;
 import it.unito.progiii.progiiiclient.utils.Constants;
 import javafx.collections.ObservableList;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class PullTask implements Runnable{
 
+    private String address;
     private StateManager stateManager;
     private ObservableList<Email> inbox;
+    private MessageBuffer request;
+    private MessageBuffer response;
 
-    public PullTask(ObservableList<Email> inbox, StateManager stateManager) {
+    public PullTask(String address,ObservableList<Email> inbox, StateManager stateManager) {
+        this.address = address;
         this.inbox = inbox;
         this.stateManager = stateManager;
+        request = new MessageBuffer();
+        response = new MessageBuffer();
+    }
+
+    private void composeRequest() {
+        request.appendData("operation=pull");
+        request.appendData("from="+address);
+
+    }
+
+    private void parse() {
+        String content = response.getContent();
+        Email[] newEmails = Constants.GSON.fromJson(content,Email[].class);
+        for(Email email:newEmails) {
+            if(!inbox.contains(email))
+                inbox.add(email);
+        }
     }
 
     @Override
     public void run() {
-        try (Socket socket = new Socket()){
-            socket.connect(new InetSocketAddress("localhost", Constants.PORT));
-            stateManager.putConnected();
-
+        try (
+                Socket socket = new Socket("localhost",Constants.PORT);
+                Scanner in = new Scanner(socket.getInputStream());
+                PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+        ){
+            composeRequest();
+            ConnectUtils.sendMessage(request,out);
+            ConnectUtils.receiveMessage(response,in);
+            parse();
+            request.clear();
+            response.clear();
         }catch (IOException e) {
             stateManager.putDisconnected();
         }
